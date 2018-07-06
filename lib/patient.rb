b4ff9b50eb5fdf4cf7ec93d5df92f76ab01d4baf
JohnMark <johnmarkludwick@gmail.com>
2018-07-06 16:17:21 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -26,7 +26,7 @@ attr_accessor :name
 
     
   def doctors
-    Doctor.all.select { |doctor| doctor.Appointment == self}
+    Doctor.all.select { |doctor| doctor.appointment == self}
   end
     
 
